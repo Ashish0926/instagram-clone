6861b42f19d165a3b5385bb3136b417f8c463daa
Ashish Gupta <ashishg0926@gmail.com>
2022-09-19 17:29:31 +0530

    connected mongodb

--- keys.js
+++ keys.js
@@ -1,4 +1,4 @@
-modules.export = {
+module.exports = {
   MONGODB_URI:
     "mongodb+srv://admin:ZiaF8dLKNOis8OjV@cluster0.txwowuj.mongodb.net/?retryWrites=true&w=majority",
 };
--- server/app.js
+++ server/app.js
@@ -1,21 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const mongoose = require("mongoose");
+const {MONGODB_URI} = require("../keys");
+
+mongoose.connect(MONGODB_URI)
+.then((db) => {
+    console.log("db connected!");
+})
+.catch((err) => {
+    console.log(err);
+})
 
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
-});
-
-// middleware --> Middleware functions are functions that have access to the request object (req), the response object (res), and the next middleware function in the application's request-response cycle. These functions are used to modify req and res objects for tasks like parsing request bodies, adding response headers, etc.
-
-const customMiddleware = (req, res, next) => {
-  console.log("middleware executed");
-  next();
-};
-
-// app.use(customMiddleware);
-
-app.get("/home", customMiddleware, (req, res) => {
-  console.log("home");
-  res.send("hello world");
 });
