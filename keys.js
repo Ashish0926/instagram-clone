864532e31c8c349699818788bf88957c85761d35
Ashish Gupta <ashishg0926@gmail.com>
2022-09-19 17:15:52 +0530

    understanding-middleware

---
+++ keys.js
@@ -0,0 +1,4 @@
+modules.export = {
+  MONGODB_URI:
+    "mongodb+srv://admin:ZiaF8dLKNOis8OjV@cluster0.txwowuj.mongodb.net/?retryWrites=true&w=majority",
+};
--- server/app.js
+++ server/app.js
@@ -6,6 +6,16 @@ app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
 
-app.get("/home", (req, res) => {
+// middleware --> Middleware functions are functions that have access to the request object (req), the response object (res), and the next middleware function in the application's request-response cycle. These functions are used to modify req and res objects for tasks like parsing request bodies, adding response headers, etc.
+
+const customMiddleware = (req, res, next) => {
+  console.log("middleware executed");
+  next();
+};
+
+// app.use(customMiddleware);
+
+app.get("/home", customMiddleware, (req, res) => {
+  console.log("home");
   res.send("hello world");
 });
