d4e83fff4e4c58a847971b927f1cf019ba8757f2
Ashish Gupta <ashishg0926@gmail.com>
2022-10-02 13:22:16 +0530

    created get method to retrieve current user post

--- server/routes/post.js
+++ server/routes/post.js
@@ -5,27 +5,51 @@ const router = express.Router();
 const requireLogin = require("../middleware/requireLogin");
 const Post = mongoose.model("Post");
 
-router.post('/createPost', requireLogin, (req, res) => {
-    const { title, body } = req.body;
-    if(!title || !body){
-        res.json({error: "Please add all the fields"});
-    }
-    //console.log(req.user);
-    res.send("Ok");
-    
-    const post = new Post({
-        title: title,
-        body: body,
-        postedBy: req.user 
+router.get("/allpost", (req, res) => {
+  Post.find()
+    .populate("postedBy", "_id name")
+    .then((posts) => {
+      res.json({ posts });
     })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
-    post.save().then(savedPost => {
-        //res.json({post: savedPost});
-        console.log(savedPost);
+router.post("/createPost", requireLogin, (req, res) => {
+  const { title, body } = req.body;
+  if (!title || !body) {
+    res.json({ error: "Please add all the fields" });
+  }
+  //console.log(req.user);
+  res.send("Ok");
+
+  const post = new Post({
+    title: title,
+    body: body,
+    postedBy: req.user,
+  });
+
+  post
+    .save()
+    .then((savedPost) => {
+      //res.json({post: savedPost});
+      console.log(savedPost);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/myposts", requireLogin, (req, res) => {
+    Post.find({postedBy: req.user._id})
+    .populate("postedBy", "_id name")
+    .then(posts => {
+        res.send(posts);
     })
     .catch(err => {
         console.log(err);
     })
-})
+});
 
 module.exports = router;
