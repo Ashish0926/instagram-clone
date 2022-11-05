a5a7820ba9757c5aedc55762c2546b23da176a3a
Ashish Gupta <ashishg0926@gmail.com>
2022-11-05 13:28:54 +0530

    updated navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,26 +1,38 @@
-import React from "react";
-import { Link } from 'react-router-dom'
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "../App";
 
 const Navbar = () => {
+  const { state, dispatch } = useContext(UserContext);
+  const renderList = () => {
+    if (state) {
+      return [
+        <li>
+          <Link to="/profile">PROFILE</Link>
+        </li>,
+        <li>
+          <Link to="/createPost">CREATE POST</Link>
+        </li>,
+      ];
+    } else {
+      return [
+        <li>
+          <Link to="/signup">SIGNUP</Link>
+        </li>,
+        <li>
+          <Link to="/login">LOGIN</Link>
+        </li>,
+      ];
+    }
+  };
   return (
     <nav>
       <div className="nav-wrapper white">
-        <Link to="/" className="brand-logo left">
+        <Link to={state ? "/" : "/login"} className="brand-logo left">
           Instagram
         </Link>
         <ul id="nav-mobile" className="right">
-          <li>
-            <Link to="/signup">SIGNUP</Link>
-          </li>
-          <li>
-            <Link to="/login">LOGIN</Link>
-          </li>
-          <li>
-            <Link to="/profile">PROFILE</Link>
-          </li>
-          <li>
-            <Link to="/createPost">CREATE POST</Link>
-          </li>
+          {renderList()}
         </ul>
       </div>
     </nav>
